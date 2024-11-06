36866ffad14a76bb255e7f4d0ec110d0ab07977b
Esteban <estebandeveloper1548@gmail.com>
2024-11-06 13:25:43 -0500

    Arreglo de CORS

--- Backend/src/config.js
+++ Backend/src/config.js
@@ -5,7 +5,7 @@ export const PORT = process.env.PORT || 3000;
 export const MONGODB_URI =process.env.MONGODB_URI;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173/api";
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000/api';
 export const LOCAL_URL = process.env.LOCAL_URL;
 
 
